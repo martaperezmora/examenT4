573219ed87746438695bad906f7e557dfc2c7d4d
Marta Perez <mpermor2205@g.educaand.es>
2023-01-30 12:10:16 +0100

    commit inicial

---
+++ src/main/java/com/exament4/mpm/MpmApplication.java
@@ -0,0 +1,13 @@
+package com.exament4.mpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exament4/mpm/MpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exament4.mpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
